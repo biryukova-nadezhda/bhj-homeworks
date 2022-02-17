3921c451e0203a2fecfc2eae9844737ad61d5d8a
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2022-02-17 19:35:05 +0300

    Сделаны три задачи в разделе DOM

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -7,68 +7,82 @@ book.classList.add("font-size_active")
 function resize(element) {
 
     if (element.className.includes("font-size_small")) {
+
         book.classList.remove("book_fs-big");
         book.classList.add("book_fs-small");
+
+        element.classList.add("font-size_active");
 
     } else if (element.className.includes("font-size_big")) {
         book.classList.remove("book_fs-small");
         book.classList.add("book_fs-big");
 
+        element.classList.add("font-size_active");
+
     } else {
         book.className = "book";
+
+        element.classList.add("font-size_active");
     }
 }
 
-function changeColor(element) {
+function changeFontColor(element) {
 
-    //Проверка цвета текста
     if (element.className.includes("text_color_gray")) {
         book.classList.remove("book_color-whitesmoke");
         book.classList.remove("book_color-black");
-
         book.classList.add("book_color-gray");
+
+        element.classList.add("color_active");
     }
 
     if (element.className.includes("text_color_whitesmoke")) {
         book.classList.remove("book_color-black");
         book.classList.remove("book_color-gray");
-
         book.classList.add("book_color-whitesmoke");
+
+        element.classList.add("color_active");
     }
 
     if (element.className.includes("text_color_black")) {
         book.classList.remove("book_color-whitesmoke");
         book.classList.remove("book_color-gray");
-
         book.classList.add("book_color-black");
-    }
 
-    //Проверка фона
+        element.classList.add("color_active");
+    }
+}
+
+function changeBgColor(element) {
     if (element.className.includes("bg_color_black")) {
         book.classList.remove("book_bg-white");
         book.classList.remove("book_bg-gray");
-
         book.classList.add("book_bg-black");
+
+        element.classList.add("color_active");
     }
 
     if (element.className.includes("bg_color_gray")) {
         book.classList.remove("book_bg-white");
         book.classList.remove("book_bg-black");
-
         book.classList.add("book_bg-gray");
+
+        element.classList.add("color_active");
     }
 
     if (element.className.includes("bg_color_white")) {
         book.classList.remove("book_bg-black");
         book.classList.remove("book_bg-gray");
-
         book.classList.add("book_bg-white");
-    }
 
+        element.classList.add("color_active");
+    }
 }
 
 fontSize.forEach((item) => {
+
     item.onclick = () => {
+        fontSize.forEach(e => e.classList.remove("font-size_active"));
         resize(item);
         return false
     }
@@ -76,7 +90,18 @@ fontSize.forEach((item) => {
 
 color.forEach((item) => {
     item.onclick = () => {
-        changeColor(item);
+        color.forEach(e => e.classList.remove("color_active"));
+
+        let parent = item.parentElement;
+
+        if (parent.className.includes("book__control_color")) {
+            changeFontColor(item);
+        }
+
+        if (parent.className.includes("book__control_background")) {
+            changeBgColor(item);
+        }
+
         return false
     }
 })
