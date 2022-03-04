1ca095ae43ae736d45c9ef5c47db6a3e53143f9b
Бирюкова Надежда Николаевна <90442752+biryukova-nadezhda@users.noreply.github.com>
2022-03-04 14:54:36 +0300

    Исправлена задача по интересам

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,15 +3,15 @@ let interest = Array.from(document.querySelectorAll(".interest__check"));
 interest.forEach((item) => {
     item.onclick = (event) => {
         event.preventDefoult;
-        let parent = item.closest(".interests").parentElement.querySelector(".interest__check");
+      
         let child = Array.from(item.closest(".interest").querySelectorAll(".interest__check"));
 
-        if (item.checked) {
-            parent.checked = true;
-            child.forEach((elem) => elem.checked = true)
-        } else {
-            parent.checked = false;
-            child.forEach((elem) => elem.checked = false)
+        if(!item.closest(".interests_active")) {
+        	if(item.checked === true) {
+        		child.forEach((element) => {element.checked = true})
+        	} else {
+        		child.forEach((element) => {element.checked = false})
+        	}
         }
     }
 })
